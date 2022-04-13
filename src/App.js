2666ffa22510574d6b14d70ee578ec0ea02b9d52
satyajitn <satyajit.nalavade@gmail.com>
2022-04-13 00:26:22 -0500

    added button click

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.css"
+import { useState } from "react"
 
-function App() {
+const App = () => {
+  const [username,setUsername] = useState("")
+  const [user,setUser] = useState("")
+
+  console.log(user);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="login">
+        <input 
+          type="text" 
+          placeholder="username" 
+          onChange={(e)=> setUsername(e.target.value)}
+        />
+        <button onClick={() => setUser(username)}>Login</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
